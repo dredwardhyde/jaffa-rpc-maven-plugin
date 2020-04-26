0b350cf33faeb3a1308f7dec3d96789336b9c2d2
edwardhyde <komesergey@gmail.com>
2020-04-27 00:45:40 +0300

    Refactoring

--- src/main/java/com/transport/lib/plugin/TransportPlugin.java
+++ src/main/java/com/transport/lib/plugin/TransportPlugin.java
@@ -12,28 +12,30 @@ import com.github.javaparser.ast.visitor.ModifierVisitor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import com.github.javaparser.ast.Modifier;
+
 import java.io.File;
 import java.io.PrintStream;
 import java.util.*;
 
-@Mojo(name = "generate-transport-api")
+@Mojo(name = "generate-transport-api", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 @SuppressWarnings("unused")
 public class TransportPlugin extends AbstractMojo {
 
     private static final HashMap<String, String> PRIMITIVE_TO_CLASS = new HashMap<>();
     private static final String API_ANNOTATION_NAME = "Api";
     private static final String API_CLIENT_ANNOTATION_NAME = "ApiClient";
-    private static final String API_ANNOTATION_PACKAGE = "com.transport.lib.common";
-    private static final String API_CLIENT_ANNOTATION_PACKAGE = "com.transport.lib.common";
+    private static final String API_ANNOTATION_PACKAGE = "com.transport.lib.annotations";
+    private static final String API_CLIENT_ANNOTATION_PACKAGE = "com.transport.lib.annotations";
     private static final String API_ANNOTATION_FULL = API_ANNOTATION_PACKAGE + "." + API_ANNOTATION_NAME;
     private static final String API_CLIENT_ANNOTATION_FULL = API_CLIENT_ANNOTATION_PACKAGE + "." + API_CLIENT_ANNOTATION_NAME;
     private static final String JAVA_EXTENSION = ".java";
-    private static final String REQUEST_INTERFACE_NAME = "RequestInterface";
-    private static final String REQUEST_INTERFACE_PACKAGE = "com.transport.lib.common";
-    private static final String REQUEST_INTERFACE_FULL = REQUEST_INTERFACE_PACKAGE + "." +  REQUEST_INTERFACE_NAME;
+    private static final String REQUEST_INTERFACE_NAME = "Request";
+    private static final String REQUEST_INTERFACE_PACKAGE = "com.transport.lib.request";
+    private static final String REQUEST_INTERFACE_FULL = REQUEST_INTERFACE_PACKAGE + "." + REQUEST_INTERFACE_NAME;
 
     static {
         PRIMITIVE_TO_CLASS.put("int", "Integer");
@@ -47,33 +49,21 @@ public class TransportPlugin extends AbstractMojo {
         PRIMITIVE_TO_CLASS.put("char", "Character");
     }
 
-    @Parameter(property = "root-path",defaultValue = "src/main/java/")
+    @Parameter(property = "root-path", defaultValue = "src/main/java/")
     private String root;
 
-    public void execute() throws MojoExecutionException {
-        try{
-            File rootFolder = new File(root);
-            if(!rootFolder.exists()) throw new IllegalArgumentException("Root " + root + " doesn't exist");
-            if(!rootFolder.isDirectory()) throw new IllegalArgumentException("Root " + root + " is not a folder");
-            processFolder(rootFolder, getLog());
-        }catch (Exception e){
-            throw new MojoExecutionException("Error during transport api generation: ", e);
-        }
-
-    }
-
-    private static void processFolder(File folder, Log log) throws Exception{
+    private static void processFolder(File folder, Log log) throws Exception {
         File[] classes = folder.listFiles((file, name) -> name.endsWith(JAVA_EXTENSION));
-        if(classes != null){
-            for(File clazz : classes){
+        if (classes != null) {
+            for (File clazz : classes) {
                 CompilationUnit cu = JavaParser.parse(clazz);
                 Optional<String> mainClass = cu.getPrimaryTypeName();
-                if(mainClass.isPresent()){
+                if (mainClass.isPresent()) {
                     Optional<ClassOrInterfaceDeclaration> cl = cu.getInterfaceByName(mainClass.get());
-                    if(cl.isPresent()){
-                        ClassOrInterfaceDeclaration classOrInterfaceDeclaration =  cl.get();
+                    if (cl.isPresent()) {
+                        ClassOrInterfaceDeclaration classOrInterfaceDeclaration = cl.get();
                         Optional<AnnotationExpr> annotationExpr = classOrInterfaceDeclaration.getAnnotationByName(API_ANNOTATION_NAME);
-                        if(annotationExpr.isPresent()){
+                        if (annotationExpr.isPresent()) {
                             log.info("Processing " + classOrInterfaceDeclaration.getName() + JAVA_EXTENSION);
                             annotationExpr.get().setName(API_CLIENT_ANNOTATION_NAME);
                             classOrInterfaceDeclaration.setName(classOrInterfaceDeclaration.getName() + "Transport");
@@ -82,11 +72,12 @@ public class TransportPlugin extends AbstractMojo {
                             cu.accept(new ImportVisitor(), null);
                             ImportDeclaration id = new ImportDeclaration(REQUEST_INTERFACE_FULL, false, false);
                             List<ImportDeclaration> idList = new ArrayList<>();
-                            if(cu.getImports()!=null) idList.addAll(cu.getImports());
-                            if(!idList.contains(id)) cu.addImport(id);
+                            if (cu.getImports() != null) idList.addAll(cu.getImports());
+                            if (!idList.contains(id)) cu.addImport(id);
                             File result = new File(folder, classOrInterfaceDeclaration.getName() + JAVA_EXTENSION);
                             boolean created = result.createNewFile();
-                            if(!created) log.info("File " + classOrInterfaceDeclaration.getName() + JAVA_EXTENSION + " updated");
+                            if (!created)
+                                log.info("File " + classOrInterfaceDeclaration.getName() + JAVA_EXTENSION + " updated");
                             try (PrintStream stream = new PrintStream(result, "UTF-8")) {
                                 stream.println(cu.toString());
                             }
@@ -97,30 +88,42 @@ public class TransportPlugin extends AbstractMojo {
         }
 
         File[] directories = folder.listFiles(File::isDirectory);
-        if(directories != null){
-            for (File directory : directories){
+        if (directories != null) {
+            for (File directory : directories) {
                 processFolder(directory, log);
             }
         }
+    }
+
+    public void execute() throws MojoExecutionException {
+        try {
+            File rootFolder = new File(root);
+            if (!rootFolder.exists()) throw new IllegalArgumentException("Root " + root + " doesn't exist");
+            if (!rootFolder.isDirectory()) throw new IllegalArgumentException("Root " + root + " is not a folder");
+            processFolder(rootFolder, getLog());
+        } catch (Exception e) {
+            throw new MojoExecutionException("Error during transport api generation: ", e);
+        }
+
     }
 
     private static class MethodVisitor extends ModifierVisitor<Void> {
         @Override
         public Node visit(MethodDeclaration n, Void arg) {
 
-            if(n.getModifiers().contains(Modifier.STATIC) || n.getModifiers().contains(Modifier.DEFAULT)) {
+            if (n.getModifiers().contains(Modifier.STATIC) || n.getModifiers().contains(Modifier.DEFAULT)) {
                 return null;
             }
-            if(PRIMITIVE_TO_CLASS.containsKey(n.getType().asString())){
-                n.setType(REQUEST_INTERFACE_NAME + "<" +  PRIMITIVE_TO_CLASS.get(n.getType().asString())+ ">");
-            }else{
-                n.setType(REQUEST_INTERFACE_NAME + "<" +  n.getType()+ ">");
+            if (PRIMITIVE_TO_CLASS.containsKey(n.getType().asString())) {
+                n.setType(REQUEST_INTERFACE_NAME + "<" + PRIMITIVE_TO_CLASS.get(n.getType().asString()) + ">");
+            } else {
+                n.setType(REQUEST_INTERFACE_NAME + "<" + n.getType() + ">");
             }
             return n;
         }
     }
 
-    private  static class FieldVisitor extends ModifierVisitor<Void> {
+    private static class FieldVisitor extends ModifierVisitor<Void> {
         @Override
         public Node visit(VariableDeclarator declarator, Void args) {
             return null;
@@ -130,7 +133,7 @@ public class TransportPlugin extends AbstractMojo {
     public static class ImportVisitor extends ModifierVisitor<Void> {
         @Override
         public Node visit(ImportDeclaration im, Void arg) {
-            if(im.getName().asString().equals(API_ANNOTATION_FULL)) im.setName(API_CLIENT_ANNOTATION_FULL);
+            if (im.getName().asString().equals(API_ANNOTATION_FULL)) im.setName(API_CLIENT_ANNOTATION_FULL);
             return im;
         }
     }
